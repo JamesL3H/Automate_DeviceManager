bcebe9623954165309271ccfd9824937c9f44231
JamesL3H <125665544+JamesL3H@users.noreply.github.com>
2023-02-17 12:57:37 -0500

    Update Device Manager Code Trial 2.py

--- Device Manager Code Trial 2.py
+++ Device Manager Code Trial 2.py
@@ -8,6 +8,7 @@ import time
 # The way Device Manager is opened causes a popup to display.  The popup states:
 #     You are logged on as a standard user. You can view device settings in Device
 #     Manager, but you must be logged on as an administrator to make changes.
+#     Opening it this way, I can't Uninstall devices.  It has to be opened a different way.
 # '''
 # dlgBox = app.DeviceManager.child_window(title="OK", auto_id="2", control_type="Button").wrapper_object()
 # dlgBox.click_input() # Click on the OK button
@@ -15,10 +16,8 @@ import time
 # menuView = app.DeviceManager.child_window(title="View", control_type="MenuItem").wrapper_object()
 # menuView.click_input() # Clicks the View menu item
 # keyboard.send_keys('w') # Press w to select Show hidden devices
-'''
-Keyboard notes > https://pywinauto.readthedocs.io/en/latest/code/pywinauto.keyboard.html
-Mouse notes > https://pywinauto.readthedocs.io/en/latest/code/pywinauto.mouse.html
-'''
+
+# Opening this way I can Uninstall devices
 keyboard.send_keys("{LWIN down}" "r" "{LWIN up}") # Press the Winows key and R to open the Run dialog
 keyboard.send_keys("devmgmt.msc" "{ENTER}") # Type devmgmt.msc into the Run dialog then press enter
 
@@ -28,8 +27,6 @@ With Device Manager open, hover the mouse over the title bar.
 Pause Live Inspect.
 Select window 'Device Manager' to see all of its elements.
 ProcessID = 23356, open Task Manager and look at Details, sort by PID.  23356 belongs to MMC.exe
-ClassName = MMCMainFrame
-Name = Device Manager
 '''
 className = 'MMCMainFrame'
 name = 'Device Manager'
@@ -52,9 +49,10 @@ print("win = " + str(win))
 dlg = app['Device Manager']
 print("dlg = " + str(dlg))
 
+'''
+I can open Device Manager but nothing I have tried is giving the window focus so I can't do anything else.
+'''
+
 # Maximize the Device Managerwindow
 # maxWindow = app.DeviceManager.child_window(title="Maximize", control_type="Button")
 # maxWindow.click_input() # Click the maximize button
-
-# mouse.click(button='left', coords=(787, 57))
-# causing a RuntimeError: There is no active desktop required for moving mouse cursor!
